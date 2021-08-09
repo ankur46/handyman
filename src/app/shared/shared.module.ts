d16416d36b0e5c629223dc018a3692fca53e5fdc
Ankur Deshwal <ankur.deshwal@verizonmedia.com>
2021-08-09 21:09:23 +0530

    fixed css changes

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ButtonComponent } from './button/button.component';
 import { DropdownComponent } from './dropdown/dropdown.component';
-
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 const COMPONENT = [
   ButtonComponent,
   DropdownComponent
+];
+const MODULES = [
+  FontAwesomeModule
 ]
 
 @NgModule({
@@ -13,8 +16,9 @@ const COMPONENT = [
     ...COMPONENT
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    ...MODULES
   ],
-  exports: [...COMPONENT]
+  exports: [...COMPONENT,  ...MODULES]
 })
 export class SharedModule { }
